a7e7cbb81961a6873732016e1c753bcc045a2331
Egil Rønningstad <egilron@ifi.uio.no>
2024-02-20 10:31:22 +0100

    tsa trust_remote_code incomplete

--- evaluation_scripts/tsa_finetuning.py
+++ evaluation_scripts/tsa_finetuning.py
@@ -11,7 +11,10 @@ label_column_name is "tsa_tags" in the TSA dataset and  "ner_tags" in the NER da
 
 
 # %%
-from datasets import ClassLabel,  load_dataset, load_from_disk, DatasetDict, Dataset
+from datasets import  load_dataset, load_from_disk
+from typing import Optional
+from dataclasses import dataclass, field
+
 import os, sys, json
 import argparse
 from pathlib import Path
@@ -31,33 +34,197 @@ from transformers import (
     TrainingArguments,
     set_seed,
 )
-
+from ner_eval import Evaluator # Batista script
 from transformers.utils import check_min_version, send_example_telemetry
 from transformers.utils.versions import require_version
-from tsa_utils import tsa_eval
-from tsa_utils import ModelArguments, DataTrainingArguments
-from modeling_norbert import NorbertForTokenClassification
+# from tsa_utils import tsa_eval
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-os.environ["WANDB_DISABLED"] = "true"
+print("Python:", sys.version)
 print("Numpy:", np.version.version)
 print("PyTorch:", torch.__version__)
 print("Transformers:", transformers.__version__)
 
 
+# Moved this inside again to have only one file
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+
+    model_name_or_path: str = field(
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+    )
+    config_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None,
+        metadata={"help": "Where do you want to store the pretrained models downloaded from huggingface.co"},
+    )
+    model_revision: str = field(
+        default="main",
+        metadata={"help": "The specific model version to use (can be a branch name, tag name or commit id)."},
+    )
+    token: str = field(
+        default=None,
+        metadata={
+            "help": (
+                "The token to use as HTTP bearer authorization for remote files. If not specified, will use the token "
+                "generated when running `huggingface-cli login` (stored in `~/.huggingface`)."
+            )
+        },
+    )
+    use_auth_token: bool = field(
+        default=None,
+        metadata={
+            "help": "The `use_auth_token` argument is deprecated and will be removed in v4.34. Please use `token` instead."
+        },
+    )
+    trust_remote_code: bool = field(
+        default=False,
+        metadata={
+            "help": (
+                "Whether or not to allow for custom models defined on the Hub in their own modeling files. This option "
+                "should only be set to `True` for repositories you trust and in which you have read the code, as it will "
+                "execute code present on the Hub on your local machine."
+            )
+        },
+    )
+    ignore_mismatched_sizes: bool = field(
+        default=False,
+        metadata={"help": "Will enable to load a pretrained model whose head dimensions are different."},
+    )
+
+
+@dataclass
+class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+    """
+
+    task_name: Optional[str] = field(default="ner", metadata={"help": "The name of the task (ner, pos...)."})
+    dataset_name: Optional[str] = field(
+        default=None, metadata={"help": "The name of the dataset to use (via the datasets library)."}
+    )
+    dataset_config_name: Optional[str] = field(
+        default=None, metadata={"help": "The configuration name of the dataset to use (via the datasets library)."}
+    )
+    train_file: Optional[str] = field(
+        default=None, metadata={"help": "The input training data file (a csv or JSON file)."}
+    )
+    validation_file: Optional[str] = field(
+        default=None,
+        metadata={"help": "An optional input evaluation data file to evaluate on (a csv or JSON file)."},
+    )
+    test_file: Optional[str] = field(
+        default=None,
+        metadata={"help": "An optional input test data file to predict on (a csv or JSON file)."},
+    )
+    text_column_name: Optional[str] = field(
+        default=None, metadata={"help": "The column name of text to input in the file (a csv or JSON file)."}
+    )
+    label_column_name: Optional[str] = field(
+        default=None, metadata={"help": "The column name of label to input in the file (a csv or JSON file)."}
+    )
+    overwrite_cache: bool = field(
+        default=False, metadata={"help": "Overwrite the cached training and evaluation sets"}
+    )
+    preprocessing_num_workers: Optional[int] = field(
+        default=None,
+        metadata={"help": "The number of processes to use for the preprocessing."},
+    )
+    max_seq_length: int = field(
+        default=None,
+        metadata={
+            "help": (
+                "The maximum total input sequence length after tokenization. If set, sequences longer "
+                "than this will be truncated, sequences shorter will be padded."
+            )
+        },
+    )
+    pad_to_max_length: bool = field(
+        default=False,
+        metadata={
+            "help": (
+                "Whether to pad all samples to model maximum sentence length. "
+                "If False, will pad the samples dynamically when batching to the maximum length in the batch. More "
+                "efficient on GPU but very bad for TPU."
+            )
+        },
+    )
+    max_train_samples: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": (
+                "For debugging purposes or quicker training, truncate the number of training examples to this "
+                "value if set."
+            )
+        },
+    )
+    max_eval_samples: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": (
+                "For debugging purposes or quicker training, truncate the number of evaluation examples to this "
+                "value if set."
+            )
+        },
+    )
+    max_predict_samples: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": (
+                "For debugging purposes or quicker training, truncate the number of prediction examples to this "
+                "value if set."
+            )
+        },
+    )
+    label_all_tokens: bool = field(
+        default=False,
+        metadata={
+            "help": (
+                "Whether to put the label for one word on all tokens of generated by that word or just on the "
+                "one (in which case the other tokens will have a padding index)."
+            )
+        },
+    )
+    return_entity_level_metrics: bool = field(
+        default=False,
+        metadata={"help": "Whether to return all the entity levels during evaluation or just the overall ones."},
+    )
+
+    def __post_init__(self):
+        if self.dataset_name is None and self.train_file is None and self.validation_file is None:
+            raise ValueError("Need either a dataset name or a training/validation file.")
+        else:
+            if self.train_file is not None:
+                extension = self.train_file.split(".")[-1]
+                assert extension in ["csv", "json"], "`train_file` should be a csv or a json file."
+            if self.validation_file is not None:
+                extension = self.validation_file.split(".")[-1]
+                assert extension in ["csv", "json"], "`validation_file` should be a csv or a json file."
+        self.task_name = self.task_name.lower()
+
+
 hf_parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
 # default_params define what named parameters can be sent to the script
 default_params = { # Add entries here that you want to change or just track the value of
-    "model_name_or_path": "ltg/norbert3-small",
-    "dataset_name": "sentiment_analysis/tsa",
+    "model_name_or_path": "ltg/norbert3-base",
+    "dataset_name": "ltg/norec_tsa",
     "seed": 101,
-    "per_device_train_batch_size": 32,
+    "per_device_train_batch_size": 16,
     "task_name": "tsa", 
     "output_dir": "~/tsa_testing", 
     "overwrite_cache": True,
     "overwrite_output_dir": True,
     "do_train": True,
-    "num_train_epochs": 1,
-    "do_eval": False,
+    "num_train_epochs": 8,
+    "learning_rate": 2e-05,
+    "do_eval": True,
     "return_entity_level_metrics": False, # Since we use a separate evaluation script, this is better kept False
     "use_auth_token": False,
     "logging_strategy": "no", # "epoch"
@@ -67,9 +234,10 @@ default_params = { # Add entries here that you want to change or just track the 
     "load_best_model_at_end": False, # Evaluate the last epoch 
     "label_column_name": "tsa_tags",
     "disable_tqdm": False,
-    "do_predict": False,
+    "do_predict": False, # We predict manually at the end anyway
     "text_column_name": "tokens"
 }
+default_params["trust_remote_code"] =  default_params["model_name_or_path"].startswith("ltg/norbert3") # NorBert3 has custom configurations in its hf repo
 
 parser=argparse.ArgumentParser(description = "Pass the path to a json file with configuration parameters as positional argument, or pass individual settings as keyword arguments.")
 parser.add_argument("config", nargs="?")
@@ -85,9 +253,36 @@ else:
 args_dict.pop("config", None)
 # Since we are flexible with what arguments are defined, we need to convert 
 
-
-
 label_col = args_dict["label_column_name"]
+
+def f1(precision, recall):
+    if precision + recall == 0:
+        return 0.0
+    score = 2 * (precision * recall) / (precision + recall)
+    return score
+
+
+def tsa_eval(gold, predictions):
+    """Predictions and gold are lists of lists with token labels as text"""
+
+    labels = set([l for s in gold for l in s])
+    labels.remove('O') # remove 'O' label from evaluation
+    labels  = list(set([l[2:] for l in labels]))
+    sorted_labels = sorted(labels,key=lambda name: (name[1:], name[0]))
+
+    evaluator = Evaluator(gold, predictions, sorted_labels)
+    results, results_agg = evaluator.evaluate()
+
+    # print("F1 scores:")
+    for entity in results_agg:
+        prec = results_agg[entity]["strict"]["precision"]
+        rec = results_agg[entity]["strict"]["recall"]
+        # print(f"{entity}:\t{f1(prec, rec):.4f}")
+    prec = results["strict"]["precision"]
+    rec = results["strict"]["recall"]
+   # print(f"Overall score: {f1(prec, rec):.4f}")
+    return f1(prec, rec)
+
 
 def compute_metrics(p):
     predictions, labels = p
@@ -165,11 +360,14 @@ def tokenize_and_align_labels(examples):
 
 
 model_args, data_args, training_args = hf_parser.parse_dict(args_dict)
+# Define what remote code we will trust
+# trust_remote = "norbert3" in model_args.model_name_or_path
 
 text_column_name = data_args.text_column_name
 label_column_name = data_args.label_column_name
 assert data_args.label_all_tokens == False, "Our script only labels first subword token"
-dsd = load_from_disk(data_args.dataset_name)
+# dsd = load_from_disk(data_args.dataset_name)
+dsd = load_dataset(data_args.dataset_name)
 transformers.logging.set_verbosity_warning()
 # %%
 
@@ -177,9 +375,7 @@ label_list = get_label_list(dsd["train"][data_args.label_column_name]) # "tsa_ta
 label_to_id = {l: i for i, l in enumerate(label_list)}
 num_labels = len(label_list)
 labels_are_int = False
-label_list
 
-# %%
 config = AutoConfig.from_pretrained(
     model_args.config_name if model_args.config_name else model_args.model_name_or_path,
     num_labels=num_labels,
@@ -187,7 +383,7 @@ config = AutoConfig.from_pretrained(
     cache_dir=model_args.cache_dir,
     revision=model_args.model_revision,
     use_auth_token=True if model_args.use_auth_token else None,
-    trust_remote_code=True,
+    trust_remote_code=model_args.trust_remote_code,
 )
 tokenizer_name_or_path = model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path
 if config.model_type in {"gpt2", "roberta"}:
@@ -210,32 +406,19 @@ else:
 
 # %%
 # Instanciate the model
-if "norbert3" in model_args.model_name_or_path:
 
-    model = NorbertForTokenClassification.from_pretrained(
-        model_args.model_name_or_path,
-        from_tf=bool(".ckpt" in model_args.model_name_or_path),
-        config=config,
-        cache_dir=model_args.cache_dir,
-        revision=model_args.model_revision,
-        use_auth_token=True if model_args.use_auth_token else None,
-        ignore_mismatched_sizes=model_args.ignore_mismatched_sizes,
-    )
-
-
-else:
-        model = AutoModelForTokenClassification.from_pretrained(
-        model_args.model_name_or_path,
-        from_tf=bool(".ckpt" in model_args.model_name_or_path),
-        config=config,
-        cache_dir=model_args.cache_dir,
-        revision=model_args.model_revision,
-        ignore_mismatched_sizes=model_args.ignore_mismatched_sizes,
-    )
+model = AutoModelForTokenClassification.from_pretrained(
+    model_args.model_name_or_path,
+    from_tf=bool(".ckpt" in model_args.model_name_or_path),
+    config=config,
+    cache_dir=model_args.cache_dir,
+    revision=model_args.model_revision,
+    ignore_mismatched_sizes=model_args.ignore_mismatched_sizes,
+    trust_remote_code=model_args.trust_remote_code,
+)
 
 
-
-print("Our label2id:    ", label_to_id)
+# print("Our label2id:    ", label_to_id)
 
 assert (model.config.label2id == PretrainedConfig(num_labels=num_labels).label2id) or (model.config.label2id == label_to_id), "Model seems to have been fine-tuned on other labels already. Our script does not adapt to that."
 
@@ -310,12 +493,7 @@ print("\nEvaluation,",model_args.model_name_or_path)
 print()
 
 # %%
-# Predict
 
-# import pickle
-
-# Debug
-# predict_dataset = predict_dataset.select([999])
 
 trainer_predict = trainer.predict(predict_dataset, metric_key_prefix="predict")
 predictions, labels, metrics = trainer_predict
@@ -336,16 +514,9 @@ for g, pred in zip(gold,true_predictions ):
     assert len(g) == len(pred), (len(g) , len(pred))
 
 batista_f1 = tsa_eval(gold, true_predictions)
-# try:
-#     if data_args.return_entity_level_metrics:
-#         seqeval_f1 =  metrics["predict_overall_f1"]
-#     else:
-#         seqeval_f1 =  metrics["predict_f1"]
-# except:
-#     seqeval_f1 = 0
+
 print("batista_f1",batista_f1)
-# print("seqeval_f1",seqeval_f1)
-# print("Seqeval > Batista:",round(seqeval_f1 - batista_f1, 4))
+
 
 args_dict["test_f1"] = batista_f1
 
